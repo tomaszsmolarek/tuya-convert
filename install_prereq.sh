520c7d506d57d11b9621a69c475bd7986975a365
tomaszsmolarek <tomaszsmolarek@users.noreply.github.com>
2019-04-30 14:19:02 +0200

    Kali 2019.1a fixes

--- install_prereq.sh
+++ install_prereq.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+# Kali 2019.1a has some auto update process, kill it
+sudo lsof -ti /var/lib/dpkg/lock-frontend | sudo xargs --no-run-if-empty kill -9
+
 sudo apt-get update
 # sudo apt-get upgrade -y
 sudo apt-get install -y dnsmasq hostapd screen curl python3-pip python3-setuptools python3-wheel mosquitto haveged
